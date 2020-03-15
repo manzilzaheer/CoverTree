c30815c44b71ead6185853461f701a6a02234d8d
Nicholas Monath <nmonath@cs.umass.edu>
2020-03-15 18:57:58 -0400

    Fixes for python 3

--- setup.py
+++ setup.py
@@ -12,19 +12,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from distutils.core import setup, Extension
-import numpy as np
+
+from setuptools import setup, Extension
+from setuptools.command.build_ext import build_ext as _build_ext
+import sys
+
+PACKAGE_NAME = 'covertree'
+
+class build_ext(_build_ext):
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        # Prevent numpy from thinking it is still in its setup process:
+        __builtins__.__NUMPY_SETUP__ = False
+        import numpy
+        self.include_dirs.append(numpy.get_include())
 
 covertreec_module = Extension('covertreec',
-		sources = ['src/cover_tree/covertreecmodule.cxx', 'src/cover_tree/cover_tree.cpp'],
-		include_dirs=[np.get_include(), 'lib/'],
-		extra_compile_args=['-march=corei7', '-pthread', '-std=c++14'],
-		extra_link_args=['-march=corei7', '-pthread', '-std=c++14']
+        sources = ['src/cover_tree/covertreecmodule.cxx',  'src/cover_tree/cover_tree.cpp'],
+        include_dirs=['lib/'],
+        extra_compile_args=['-march=corei7-avx', '-pthread', '-std=c++14'],
+        extra_link_args=['-march=corei7-avx', '-pthread', '-std=c++14'] #'-DPRINTVER']
+)
+setup ( name = 'covertree',
+    version = '1.0',
+    description = 'Cover Tree -- fast search',
+    cmdclass={'build_ext':build_ext},
+    install_requires=['numpy>=1.13.1', 'scipy>=0.17', 'scikit-learn>=0.18.1'],
+    ext_modules = [ covertreec_module, ],
+    packages = ['covertree']
 )
 
-setup ( name = 'covertreec',
-	version = '1.0',
-	description = 'Cover Tree Data Structure.',
-	ext_modules = [ covertreec_module ],
-	packages = ['covertree']
-)
+
